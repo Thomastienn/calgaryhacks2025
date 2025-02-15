5291c9d60ab4f32daee6aa549283d737d22a5ed7
thomas <thomas.vu1@ucalgary.ca>
2025-02-15 12:23:23 -0700

    Done the base finance

--- src/Finance/FinanceTracker.py
+++ src/Finance/FinanceTracker.py
@@ -15,13 +15,37 @@ class FinanceTracker:
         self.finance_list[year][month][day].append(Thing(name=thing, amount=money,type=category))
     def getAll(self) -> dict: 
         return self.finance_list
-    def expenseSpecificMonth(self, year:int, month: int) -> float:
-        this_month = self.finance_list[year][month]
+    
+    def expenseSpecificDay(self, year:int, month:int, day:int) -> float:
         total = 0
-        for day in this_month:
-            for stuff in day:
-                if stuff.isEssential():
+        for stuff in self.finance_list[year][month][day]:
+            if stuff.isEssential():
+                total += stuff.getAmount()
+        return total
+    
+    def expenseSpecificMonth(self, year:int, month: int) -> float:
+        total = 0
+        for day in self.finance_list[year][month]:
+            total += self.expenseSpecificDay(year, month, day)
+        return total
+    def expenseSpecificYear(self, year:int) -> float:
+        total = 0
+        for month in self.finance_list[year]:
+            total += self.expenseSpecificMonth(year, month)
+        return total
+    
+    def incomeThisMonth(self, year:int, month:int) -> float:
+        total = 0
+        for day in self.finance_list[year][month]:
+            for stuff in self.finance_list[year][month][day]:
+                if stuff.type == Thing.INCOME:
                     total += stuff.getAmount()
+        return total
+        
+    def incomeThisYear(self, year:int) -> float:
+        total = 0
+        for month in self.finance_list[year]:
+            total += self.incomeThisMonth(year, month)
         return total
 
                 

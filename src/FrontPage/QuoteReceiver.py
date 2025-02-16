d0b3f662644aed3f3d184d27573ec9ab3b5527ff
thomas <thomas.vu1@ucalgary.ca>
2025-02-15 22:56:02 -0700

    A

--- src/FrontPage/QuoteReceiver.py
+++ src/FrontPage/QuoteReceiver.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from Quote import Quote
+from src.FrontPage.Quote import Quote
 class QuoteReceiver:
     def __init__(self):
         self.LINK = "https://qapi.vercel.app/api/random"
@@ -8,3 +8,4 @@ class QuoteReceiver:
     def getQuote(self):
         data = json.loads(requests.get(self.LINK).text)
         return Quote(desc=data["quote"], author=data["author"])
+    
